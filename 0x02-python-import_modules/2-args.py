01e39972dc875031976b6ad7580d9c53c95b0e95
Redemtourist <anyalemechi.jonathan@gmail.com>
2022-09-24 08:16:52 -0700

    a program that prints #pythoniscool, followed by a new line, in the standard output.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(num - 1))
         for i in range(1, num):
-            print("{}: {}".format(i, sys.argv[i]))
+    print("{}: {}".format(i, sys.argv[i]))
